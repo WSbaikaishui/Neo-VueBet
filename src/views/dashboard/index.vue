e1f7820abea5bacd7900a4551bbe6c24eea1b26a
WSbaikaishui <46080358+WSbaikaishui@users.noreply.github.com>
2021-07-07 15:17:44 +0800

    first commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dashboard-container">
+
+
+
+
+  <div class="zyw">Hello</div>
+    <div class="dashboard-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+  .zyw{
+    font-size: 800px;
+  }
+}
+</style>
